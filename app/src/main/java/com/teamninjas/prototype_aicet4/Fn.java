d2d79c464c3f7164429caee7a312a6fa4e5e0677
Apekshit Tewathia <apekshittewathia@yahoo.com>
2017-01-04 18:49:37 +0530

    first COMMIT

---
+++ app/src/main/java/com/teamninjas/prototype_aicet4/Fn.java
@@ -0,0 +1,98 @@
+package com.teamninjas.prototype_aicet4;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+/**
+ * Created by Apekshit on 04-01-2017.
+ */
+
+public class Fn {
+    public static String decodeFile(String path,int DESIREDWIDTH, int DESIREDHEIGHT) {
+        String strMyImagePath = null;
+        Bitmap scaledBitmap = null;
+
+        try {
+            // Part 1: Decode image
+            Bitmap unscaledBitmap = ScalingUtilities.decodeFile(path, DESIREDWIDTH, DESIREDHEIGHT, ScalingUtilities.ScalingLogic.FIT);
+
+            if ((unscaledBitmap.getWidth() >= DESIREDWIDTH) || (unscaledBitmap.getHeight() >= DESIREDHEIGHT)) {
+                // Part 2: Scale image
+                scaledBitmap = ScalingUtilities.createScaledBitmap(unscaledBitmap, DESIREDWIDTH, DESIREDHEIGHT, ScalingUtilities.ScalingLogic.FIT);
+              //  Fn.logD("returning_scaled_image","returning_scaled_image");
+            } else {
+                unscaledBitmap.recycle();
+              //  Fn.logD("returning_original_imag","returning_original_imag");
+                return path;
+            }
+
+            // Store to tmp file
+
+            String extr = Environment.getExternalStorageDirectory().toString();
+            File mFolder = new File(extr + "/TMMFOLDER");
+            if (!mFolder.exists()) {
+                mFolder.mkdir();
+            }
+
+            String s = "tmp.png";
+
+            File f = new File(mFolder.getAbsolutePath(), s);
+
+            strMyImagePath = f.getAbsolutePath();
+            FileOutputStream fos = null;
+            try {
+                fos = new FileOutputStream(f);
+                scaledBitmap.compress(Bitmap.CompressFormat.JPEG, 75, fos);
+                fos.flush();
+                fos.close();
+            } catch (FileNotFoundException e) {
+
+                e.printStackTrace();
+            } catch (Exception e) {
+
+                e.printStackTrace();
+            }
+
+            scaledBitmap.recycle();
+        } catch (Throwable e) {
+        }
+
+        if (strMyImagePath == null) {
+            return path;
+        }
+        return strMyImagePath;
+
+    }
+    public static Uri getImageContentUri(Context context, String filePath) {
+//        String filePath = imageFile.getAbsolutePath();
+        File imageFile = new File(filePath);
+        Cursor cursor = context.getContentResolver().query(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                new String[] { MediaStore.Images.Media._ID },
+                MediaStore.Images.Media.DATA + "=? ",
+                new String[] { filePath }, null);
+        if (cursor != null && cursor.moveToFirst()) {
+            int id = cursor.getInt(cursor.getColumnIndex(MediaStore.MediaColumns._ID));
+            cursor.close();
+            return Uri.withAppendedPath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "" + id);
+        } else {
+            if (imageFile.exists()) {
+                ContentValues values = new ContentValues();
+                values.put(MediaStore.Images.Media.DATA, filePath);
+                return context.getContentResolver().insert(
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+            } else {
+                return null;
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/teamninjas/prototype_aicet4/collegeitem.java
@@ -0,0 +1,17 @@
+package com.teamninjas.prototype_aicet4;
+
+import android.widget.TextView;
+
+/**
+ * Created by Apekshit on 04-01-2017.
+ */
+
+public class collegeitem {
+    boolean checked=false;
+    TextView textView;
+
+    collegeitem(){
+    }
+
+
+}

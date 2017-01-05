6faebbad5b0ec99ef7338ebcd95e79a7728d9d0e
Shivam Arora <shivam_arora@live.in>
2017-01-06 01:43:17 +0530

    FCM COMMIT

---
+++ app/src/main/java/com/teamninjas/prototype_aicet4/Services/FCM/MessagingService.java
@@ -0,0 +1,11 @@
+package com.teamninjas.prototype_aicet4.Services.FCM;
+
+import android.app.Service;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+/**
+ * Created by ShivamArora on 05-01-2017.
+ */
+public class MessagingService extends FirebaseMessagingService {
+}
